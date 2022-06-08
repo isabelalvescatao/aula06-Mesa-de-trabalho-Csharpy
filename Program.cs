4051dd4b93d7ffd1d60cc092222d841fb2b8be49
Isabel Catão <86860952+isabelalvescatao@users.noreply.github.com>
2022-06-08 16:38:47 -0300

    Começo do Projeto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Utilize a mensagem a seguir como base do retorno esperado:
+// Olá João, você nasceu em XXXX.
+
+string nomeDoUsuario;
+int idadeDoUsuario;
+int anoAtual = 2022;
+int anoNascimento;
+
+Console.WriteLine("Qual seu nome? ");
+nomeDoUsuario = Console.ReadLine();
+Console.WriteLine("Qual sua idade? ");
+idadeDoUsuario = int.Parse(Console.ReadLine());
+
+anoNascimento = anoAtual - idadeDoUsuario;
+
+
+Console.Write($"Olá {nomeDoUsuario}, você nasceu em {anoNascimento}!");
